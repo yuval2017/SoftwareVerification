ba5c9b6ace3c6259488890b4e59a5e23d4e7e7ed
ywblhytr <hitter@post.bgu.ac.il>
2022-11-26 22:21:38 +0200

    first ass5 commit finished - grade is 100

--- ass5/solution.py
+++ ass5/solution.py
@@ -21,6 +21,18 @@ def evaluate(cond, eta):
     return eval(cond, None, eta)
 
 
+ts1 = {'S': {'s1', 's3', 's2'},
+       'Act': {'b', 'd', 'a'},
+       'to': {('s2', 'd', 's3'), ('s1', 'a', 's2'), ('s2', 'b', 's1')},
+       'I': {'s1'}, 'AP': {'b', 'a'},
+       'L': {}}
+ts2 = {'S': {'s4', 's5'},
+       'Act': {'b', 'a', 'c'},
+       'to': {('s4', 'a', 's5'), ('s5', 'b', 's5'), ('s5', 'c', 's4')},
+       'I': {'s4'}, 'AP': {'b', 'a'},
+       'L': {}}
+
+
 def transition_system_from_program_graph(pg, vars, labels):
     Loc = pg.get('Loc')
     Loc0 = pg.get('Loc0')
@@ -39,16 +51,19 @@ def create_to_and_S(I, to1, to2, h, Act):
             S.add(initial)
             for a in Act:
                 if a in h:
-                    sub_to1 = filter(lambda x: x[0] == initial[0] and x[1] == a, to1)
-                    sub_to2 = filter(lambda x: x[0] == initial[1] and x[1] == a, to2)
-                    to_list = list(product(sub_to1, sub_to2))
-                    post = post + [(i[0][2], i[1][2]) for i in to_list]
+                    sub_to1 = list(filter(lambda x: x[0] == initial[0] and x[1] == a, to1))
+                    sub_to2 = list(filter(lambda x: x[0] == initial[1] and x[1] == a, to2))
+                    t = list(product(sub_to1, sub_to2))
+                    curr = [(i[0][2], i[1][2]) for i in t]
+                    post = post + curr
+
                 else:
                     sub_to1 = list(filter(lambda x: x[0] == initial[0] and x[1] == a, to1))
-                    post = post + [(i[2], initial[1]) for i in sub_to1]
                     sub_to2 = list(filter(lambda x: x[0] == initial[1] and x[1] == a, to2))
-                    post = post + [(initial[0], i[2]) for i in sub_to2]
-                ans = ans + [(initial, a, i) for i in post]
+                    curr = [(initial[0], i[2]) for i in sub_to2] + [(i[2], initial[1]) for i in sub_to1]
+                    post = post + curr
+
+                ans = ans + [(initial, a, i) for i in curr]
 
             for p in post:
                 t = help_dfs(p, to1, to2, S, h)
@@ -66,56 +81,84 @@ def interleave_transition_systems(ts1, ts2, h):
     Act1 = ts1.get('Act')
     to1 = ts1.get('to')
     I1 = ts1.get('I')
+    AP1 = ts1.get('AP')
     L1 = ts1.get('L')
 
     Act2 = ts2.get('Act')
     to2 = ts2.get('to')
     I2 = ts2.get('I')
+    AP2 = ts2.get('AP')
     L2 = ts2.get('L')
 
-    I = product(I1, I2)
+    I = set(product(I1, I2))
     S, to = create_to_and_S(I, to1, to2, h, Act1.union(Act2))
     Act = Act1.union(Act2)
+    AP = AP1.union(AP2)
     L = lambda s: L1(s[0]).union(L2(s[1]))
-    return {'S': S, 'Act': Act, 'to': to, 'I': I, 'L': L}
+    return {'S': S, 'Act': Act, 'to': to, 'I': I, 'AP': AP, 'L': L}
+
+
+peterson0 = {'Loc': {'crit', 'noncrit', 'wait'},
+             'Act': {'', 'b0=True;x=1', 'b0=False'},
+             'Eval': evaluate,
+             'Effect': effect,
+             'to': {('noncrit', 'True', 'b0=True;x=1', 'wait'), ('wait', 'x==0 or not b1', '', 'crit'),
+                    ('crit', 'True', 'b0=False', 'noncrit')},
+             'Loc0': {'noncrit'},
+             'g0': 'not b0'}
+peterson1 = {'Loc': {'crit', 'noncrit', 'wait'},
+             'Act': {'', 'b1=True;x=0', 'b1=False'},
+             'Eval': evaluate,
+             'Effect': effect,
+             'to': {('noncrit', 'True', 'b1=True;x=0', 'wait'), ('crit', 'True', 'b1=False', 'noncrit'),
+                    ('wait', 'x==1 or not b0', '', 'crit')},
+             'Loc0': {'noncrit'},
+             'g0': 'not b1'}
+
+
+def create_to_and_lock(Loc0, to1, to2):
+    def help_dfs(initial, to1, to2, Loc):
+        ans = []
+        if initial not in Loc:
+            Loc.add(initial)
+            sub_to1 = list(filter(lambda x: x[0] == initial[0], to1))
+            sub_to2 = list(filter(lambda x: x[0] == initial[1], to2))
+            post = [(i[3], initial[1]) for i in sub_to1] + [(initial[0], i[3]) for i in sub_to2]
+            ans = [(initial, i[1], i[2], (i[3], initial[1])) for i in sub_to1] + [
+                (initial, i[1], i[2], (initial[0], i[3])) for i in sub_to2]
+            for p in post:
+                ans = ans + help_dfs(p, to1, to2, Loc)
+        return ans
+
+    Loc = set()
+    to = []
+    for l in Loc0:
+        to = to + help_dfs(l, to1, to2, Loc)
+    return Loc, set(to)
 
 
 def interleave_program_graphs(pg1, pg2):
-    Lock1 = pg1.get('Lock')
     Act1 = pg1.get('Act')
     Eval1 = pg1.get('Eval')
     Effect1 = pg1.get("Effect")
     to1 = pg1.get('to')
-    Lock01 = pg1.get('Lock0')
+    Loc01 = pg1.get('Loc0')
     g01 = pg1.get('g0')
     # 2 nd pg
-    Lock2 = pg2.get('Lock')
     Act2 = pg2.get('Act')
     Eval2 = pg2.get('Eval')
     Effect2 = pg2.get("Effect")
-    to1 = pg2.get('to')
-    Lock02 = pg2.get('Lock0')
+    to2 = pg2.get('to')
+    Loc02 = pg2.get('Loc0')
     g02 = pg2.get('g0')
 
-    Lock = product(Lock01, Lock02)
+    Loc0 = set(product(Loc01, Loc02))
     Act = Act1.union(Act2)
-    Effect1 = Effect1
-    g0 = g01 + 'and' + g02
-
-
-
-
-
-ts1 = {'S': {'s1', 's3', 's2'},
-       'Act': {'b', 'd', 'a'},
-       'to': {('s2', 'd', 's3'), ('s1', 'a', 's2'), ('s2', 'b', 's1')},
-       'I': {'s1'}, 'AP': {'b', 'a'},
-       'L': {}}
-ts2 = {'S': {'s4', 's5'},
-       'Act': {'b', 'a', 'c'},
-       'to': {('s4', 'a', 's5'), ('s5', 'b', 's5'), ('s5', 'c', 's4')},
-       'I': {'s4'}, 'AP': {'b', 'a'},
-       'L': {}}
+    Eval = Eval1
+    Effect = Effect1
+    g0 = g01 + ' and ' + g02
+    Loc, to = create_to_and_lock(Loc0, to1, to2)
+    return {'Loc': Loc, 'Act': Act, 'Eval': Eval, 'Effect': Effect, 'to': to, 'Loc0': Loc0, 'g0': g0}
 
 
 def print_hi(name):
@@ -125,5 +168,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    interleave_transition_systems(ts1, ts2, {'a', 'b'})
+    i = interleave_program_graphs(peterson0, peterson1)
+    t = interleave_transition_systems(ts1, ts2, {'a', 'b'})
+
     j = 1
